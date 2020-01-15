40922a0eb7e1b5e9ef913f50a6e4ce47af639c5c
hermolaou <31070526+hermolaou@users.noreply.github.com>
2020-01-15 16:10:55 +0300

    Add files via upload
    Это включаемые файлы на JavaScript и VBScript с некоторыми функциями общего назначения, которые удобно подключать к приложениям HTA. Хотелось бы, чтобы кто-нибудь перевёл код с VBScript на JavaScript.
    These are JavaScript and VBscript include files with some general-purpose functions, convenient to be included in HTAs. It would be great if someone converted VBScript code to JavaScript.

---
+++ sql.js
@@ -0,0 +1,64 @@
+/*
+Doxa soi, Kyrie, doxa soi.
+*/
+
+var queries = [];	//queue
+
+function SqlAsync(q, OnCompletion)
+{
+	//add to queue
+	if (typeof conn=="undefined")
+		SqlConnect (dbFile)
+	
+	if (typeof q == "string")
+		queries.push({q:ReplaceExtChars(q), callback:OnCompletion});
+	else if (typeof q=="object")
+		q.forEach(function(query) {
+			queries.push({q:ReplaceExtChars(query), callback:OnCompletion});
+		});
+	
+	if (conn.State==(adStateOpen | adStateExecuting))
+		return; //Query in progress.
+		
+	//if queue not empty then run the next query
+	if (queries.length==0) return;
+
+	//get from queue
+	q=queries.shift();
+	
+	var rs=conn.Execute (q.q, null, adAsyncExecute);
+		
+	var sqlInterval=self.setInterval (function () {
+		if (rs.state==adStateExecuting) return;
+		
+		self.clearInterval (sqlInterval);
+		
+		if (conn.Errors.Count) {
+			echo ("adodb errors count:", conn.Errors.Count);
+			
+			alert (format("ADODB error:", conn.Errors(0).Description, "\n", "Query: ", q.q));					
+		
+		} else {
+						
+			//may the recordset be closed at this point?
+			if (rs.State==adStateClosed)
+				alert ("Recordset arrived closed. This is strange.\n" + q.q);
+			else
+				q.callback(rs, q.q);
+			
+			rs.Close();
+			
+		}	
+		
+		SqlAsync();
+	
+	}, 50);
+
+}
+
+/*
+queue.push(2);         // queue is now [2]
+queue.push(5);         // queue is now [2, 5]
+var i = queue.shift(); // queue is now [5]
+alert(i);              // displays 2
+*/
